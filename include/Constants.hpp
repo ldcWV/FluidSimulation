363ba67d2317c34e7d894bddd5b9ecbd635b871b
ldcWV <ldorianchen@gmail.com>
2022-11-30 01:55:52 -0500

    scene file changes

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -3,7 +3,7 @@
 
 namespace Constants {
     const glm::dvec3 g = {0, -9.8, 0};
-    const double dt = 1.0 / 60.0;
+    const double dt = 1.0 / 60.0 * 0.2;
     const double eps = 1e5;
     const double mass = 1.0 / 30.0;
     const double radius = 0.1;
