d15db71d7ec4a80d3547d03fd607b0bc84e98e03
ldcWV <ldorianchen@gmail.com>
2022-11-29 20:29:10 -0500

    Updating particles via gravity

--- src/SequentialSimulator.cpp
+++ src/SequentialSimulator.cpp
@@ -1,7 +1,12 @@
 #include "Simulator.hpp"
+#include "Constants.hpp"
 
 SequentialSimulator::SequentialSimulator() {}
 
 void SequentialSimulator::update(double elapsed, Scene& scene) {
-    return;
+    // Apply gravity on all particles
+    for (auto& p : scene.particles) {
+        p.vel += elapsed * Constants::g;
+        p.pos += elapsed * p.vel;
+    }
 }
