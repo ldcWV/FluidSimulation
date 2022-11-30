5bef2fb1477e7002fa180c9a60c4b2d48ff39f8c
ldcWV <ldorianchen@gmail.com>
2022-11-29 23:08:11 -0500

    poly6

--- include/Kernels.hpp
+++ include/Kernels.hpp
@@ -3,7 +3,11 @@
 #include <math.h>
 
 namespace Kernels {
-    double poly6(const glm::dvec3& r, const double h);
+    double poly6(const glm::dvec3& r, const double h) {
+        double r_mag = glm::length(r);
+        if (r_mag > h) 0;
+        return 315.0 / (64 * Constants::pi * pow(h, 9)) * pow(h*h - r_mag*r_mag, 3);
+    }
 
     glm::dvec3 gradSpiky(const glm::dvec3& r, const double h) {
         double r_mag = glm::length(r);
